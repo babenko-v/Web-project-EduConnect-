455b574bf15e683242166cd5493537193c7d52e9
Babenko Valera <11112004valerababenko@gmail.com>
2024-06-18 21:53:29 +0300

    minor bux fix

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+
 from django.contrib.auth.models import AbstractUser
+from django.db.models import IntegerField
 
 from teachers.models import Specializations
 
@@ -10,7 +12,12 @@ class Locations(models.Model):
         db_table = 'location'
         ordering = ['name',]
 
+    def __str__(self):
+        return self.name
+
 class Contact_info(models.Model):
+    phone = IntegerField(verbose_name='phone number')
+    email = models.EmailField(blank=True, max_length=254, verbose_name='email address')
     telegram = models.CharField(max_length=50, unique=True, blank=True, null=True)
     facebook = models.CharField(max_length=100, unique=True, blank=True, null=True)
     instagram = models.CharField(max_length=50, unique=True, blank=True, null=True)
@@ -18,11 +25,20 @@ class Contact_info(models.Model):
     class Meta:
         db_table = 'contact'
 
+    def __str__(self):
+        return self.email
+
+
 class Mode_teaching(models.Model):
     name_mode = models.CharField(max_length=20, unique=True, verbose_name='mode_teaching')
 
     class Meta:
         db_table = 'mode_of_teaching'
+
+    def __str__(self):
+        return self.name_mode
+
+
 class Teacher_profile(AbstractUser):
     age = models.PositiveIntegerField()
     experience = models.PositiveIntegerField(default=0)
@@ -35,6 +51,9 @@ class Teacher_profile(AbstractUser):
     contact_info = models.ForeignKey(to=Contact_info, on_delete=models.CASCADE)
 
     image = models.ImageField(upload_to='users_images', blank=True, null=True, verbose_name='profile_image')
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name}'
 
 
 
