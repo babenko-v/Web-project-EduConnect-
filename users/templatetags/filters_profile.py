1670ef8260fe705eb0f93d01e63ce5aff244e8a7
Babenko Valera <11112004valerababenko@gmail.com>
2024-07-12 18:23:30 +0300

    fix filter

--- users/templatetags/filters_profile.py
+++ users/templatetags/filters_profile.py
@@ -1,4 +1,3 @@
-
 from django import template
 from django.utils.safestring import mark_safe
 
@@ -7,10 +6,19 @@ register = template.Library()
 
 @register.filter(name='default_no_info')
 def default_no_info(value):
-    if value != "None" and value:
-        if isinstance(value, int) and value < 100:
-            return f"{value} years"
+    try:
+        if value is None or value == "":
+            return mark_safe("<i>Information not provided</i>")
+
+        int_value = int(value)
+
+        if int_value < 100:
+            return f"{int_value} years"
+
         return value
-    else:
-        return mark_safe("<i>Information not provided</i>")
+    except (ValueError, TypeError):
+
+        return value
+
+
 
