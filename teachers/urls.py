4f6b7cf04544999c6266263806ff236cb9b0c645
Babenko Valera <11112004valerababenko@gmail.com>
2024-07-12 21:09:12 +0300

    change listview in roster teacher controllers

--- teachers/urls.py
+++ teachers/urls.py
@@ -4,7 +4,7 @@ from teachers import views
 app_name = 'teachers'
 
 urlpatterns = [
-    path('',  views.roster, name='roster'),
-    path('search/',  views.roster, name='search'),
+    path('',  views.RosterView.as_view(), name='roster'),
+    path('search/',  views.RosterView.as_view(), name='search'),
     path('forms/<int:mentors_id>/', views.MentorView.as_view(), name='mentors'),
 ]
--- teachers/views.py
+++ teachers/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 from teachers.models import Specializations
 from users.models import Locations, Mode_teaching, Teacher_profile
@@ -8,61 +8,97 @@ from teachers.utils import search_query
 from django.shortcuts import render
 
 
-def roster(request):
+class RosterView(ListView):
+    model = Teacher_profile
+    template_name = 'teachers/roster_mentors.html'
+    context_object_name = 'profiles'
+    paginate_by = 3
 
-    modes = Mode_teaching.objects.all()
-    locality = Locations.objects.all()
-    specializations = Specializations.objects.all()
+    def get_queryset(self):
+        queryset = super().get_queryset().exclude(is_staff=True)
 
-    query = request.GET.get('q')
+        query = self.request.GET.get('q')
+        type_std = self.request.GET.get('type_std')
+        speciality = self.request.GET.get('speciality')
+        location = self.request.GET.get('location')
+        sort = self.request.GET.get('sort')
 
-    profiles = Teacher_profile.objects.all()
-    profiles = profiles.exclude(is_staff=True)
+        if query:
+            queryset = search_query(query)
+        if speciality:
+            queryset = queryset.filter(main_specialty__name_spec=speciality)
+        if type_std:
+            queryset = queryset.filter(mode_teaching__name_mode=type_std)
+        if location:
+            queryset = queryset.filter(locations__name=location)
+        if sort:
+            queryset = queryset.order_by(sort)
 
-    type_std = request.GET.get('type_std')
-    speciality = request.GET.get('speciality')
-    location = request.GET.get('location')
-    sort = request.GET.get('sort')
+        return queryset
 
-    if query:
-        profiles = search_query(query)
-    else:
-        ...
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['profiles_amount'] = self.get_queryset().count()
+        context['modes'] = Mode_teaching.objects.all()
+        context['locality'] = Locations.objects.all()
+        context['specializations'] = Specializations.objects.all()
+        return context
 
-    if speciality:
-        profiles = profiles.filter(main_specialty__name_spec=speciality)
-
-    if type_std:
-        profiles = profiles.filter(mode_teaching__name_mode=type_std)
-
-    if location:
-        profiles = profiles.filter(locations__name=location)
-
-    if sort:
-        profiles = profiles.order_by(sort)
-
-    # Пагинация
-    paginator = Paginator(profiles, 3)  # Количество элементов на странице
-    page_number = request.GET.get('page', 1)
-
-
-    try:
-        current_page = paginator.page(int(page_number))
-    except PageNotAnInteger:
-        current_page = paginator.page(1)
-    except EmptyPage:
-        current_page = paginator.page(paginator.num_pages)
-
-    context = {
-        'profiles_amount': profiles.count(),  # Количество профилей, удовлетворяющих фильтрам
-        'profiles': current_page,
-        'modes': modes,
-        'locality': locality,
-        'specializations': specializations,
-
-    }
-
-    return render(request, "teachers/roster_mentors.html", context)
+# def roster(request):
+#
+#     modes = Mode_teaching.objects.all()
+#     locality = Locations.objects.all()
+#     specializations = Specializations.objects.all()
+#
+#     query = request.GET.get('q')
+#
+#     profiles = Teacher_profile.objects.all()
+#     profiles = profiles.exclude(is_staff=True)
+#
+#     type_std = request.GET.get('type_std')
+#     speciality = request.GET.get('speciality')
+#     location = request.GET.get('location')
+#     sort = request.GET.get('sort')
+#
+#     if query:
+#         profiles = search_query(query)
+#     else:
+#         ...
+#
+#     if speciality:
+#         profiles = profiles.filter(main_specialty__name_spec=speciality)
+#
+#     if type_std:
+#         profiles = profiles.filter(mode_teaching__name_mode=type_std)
+#
+#     if location:
+#         profiles = profiles.filter(locations__name=location)
+#
+#     if sort:
+#         profiles = profiles.order_by(sort)
+#
+#     # Пагинация
+#     paginator = Paginator(profiles, 3)  # Количество элементов на странице
+#     page_number = request.GET.get('page', 1)
+#
+#
+#     try:
+#         current_page = paginator.page(int(page_number))
+#     except PageNotAnInteger:
+#         current_page = paginator.page(1)
+#     except EmptyPage:
+#         current_page = paginator.page(paginator.num_pages)
+#
+#     context = {
+#         'profiles_amount': profiles.count(),  # Количество профилей, удовлетворяющих фильтрам
+#         'profiles': current_page,
+#         'modes': modes,
+#         'locality': locality,
+#         'specializations': specializations,
+#
+#     }
+#
+#     return render(request, "teachers/roster_mentors.html", context)
 
 # def mentors(request, mentors_id):
 #     profiles = Teacher_profile.objects.filter(id=mentors_id)
